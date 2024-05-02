3b255ea930303b457266b3ff0892d4a1bce4d0d7
jessvand <jmvandbeek@gmail.com>
2024-05-02 15:04:46 -0400

    Favorite subjects/ case

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi! What's your favorite subject?");
+
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+               case "math":
+                    Console.WriteLine("I'm also TERRIBLE at math!");
+                    break;
+               case "english":
+                    Console.WriteLine("I really love reading and have a passion for etymology");
+                    break;
+                case "chemistry":
+                    Console.WriteLine("I only understand it now as an adult...because, DANGER!");
+                        break;
+                case "taylor swift":
+                    Console.WriteLine("Its wild that there are uni courses on Taylor Swift");
+                    break;
+                case "weightlifting":
+                    Console.WriteLine("this is the absolute BEST class of them all!");
+                    break;
+
+
+              
+                       
+            }                
         }
     }
 }
